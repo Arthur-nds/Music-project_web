8a0f4f1ed915e1c3db62ddb124df086f189210e6
Arthur-nds <arthurotp5@gmail.com>
2026-01-19 23:26:08 -0300

    Forms de adicionar músicas.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
+// Manipulação do DOM --> musicas.html
 const addMusic = document.querySelector("#addMusic");
 
-addMusic.addEventListener('click' , function () {
-    const glass = 
+addMusic.addEventListener('click', () => {
+    const templateForm = document.querySelector('#addMusic-template');
+    const addMusicForm = templateForm.content.cloneNode(true);
+    document.body.appendChild(addMusicForm);
+    document.querySelector('.close-crud').addEventListener('click', () => {
+        const popup = document.querySelector('.pop-up');
+        popup.remove();
+    });
 });
+
